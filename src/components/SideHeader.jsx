da68333fd9e082684f190e74c365fb90f66ed10f
AndyCotruta <acotruta18@gmail.com>
2023-02-21 11:26:55 +0100

    SideHeader complete

--- src/components/SideHeader.jsx
+++ src/components/SideHeader.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { ReactComponent as Profile } from "./icons/profile.svg";
+import { ReactComponent as Group } from "./icons/group.svg";
+import { ReactComponent as Circle } from "./icons/circle.svg";
+import { ReactComponent as Chat } from "./icons/chat.svg";
+import { ReactComponent as ThreeDots } from "./icons/threeDots.svg";
 
 function SideHeader() {
-  return <div>SideHeader</div>;
+  return (
+    <div className="side-header d-flex justify-content-between">
+      <Profile className="my-icon" />
+
+      <div className="d-flex align-items-center">
+        <div className="header-icon">
+          <Group />
+        </div>
+
+        <div className="header-icon">
+          <Circle />
+        </div>
+
+        <div className="header-icon">
+          <Chat />
+        </div>
+
+        <div className="header-icon">
+          <ThreeDots />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SideHeader;
