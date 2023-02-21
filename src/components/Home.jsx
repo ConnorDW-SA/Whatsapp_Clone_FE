81157da0c2e565ae4dd18f74094f9511f3761170
ConnorDW-SA <114667779+ConnorDW-SA@users.noreply.github.com>
2023-02-21 11:33:40 +0200

    .

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,7 @@ import MainView from "./MainView";
 function Home() {
   return (
     <div className="home d-flex">
+      <img src="./stolen/phone.svg" alt="phone" className="phone" />
       <SideView />
       <MainView />
     </div>
