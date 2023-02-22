7d32212053eace5516823bb3f6b23444aa1c024e
ConnorDW-SA <114667779+ConnorDW-SA@users.noreply.github.com>
2023-02-22 14:45:14 +0200

    .

--- src/components/SingleChat/MainView/MessagesList.jsx
+++ src/components/SingleChat/MainView/MessagesList.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-
 import MessagePersonOne from "../Chats/PersonOne.json";
 import MessagePersonTwo from "../Chats/PersonTwo.json";
+
+function formatTime(timeString) {
+  const date = new Date(timeString);
+  const hour = date.getHours().toString().padStart(2, "0");
+  const minute = date.getMinutes().toString().padStart(2, "0");
+  return `${hour}:${minute}`;
+}
 
 function MessagesList() {
   let currentSender = "";
@@ -14,25 +20,32 @@ function MessagesList() {
     <div className="d-flex flex-column convo-div">
       {messages.map((message, index) => {
         const sender = MessagePersonOne.includes(message)
-          ? "person-one"
-          : "person-two";
+          ? "person-one-messages"
+          : "person-two-messages";
         const isNewSender = currentSender !== sender;
         currentSender = sender;
 
         const messageClasses = `message-container p-2 m-1 text-wrap rounded-lg ${sender} ${
-          sender === "person-one" ? "bg-message-whatsapp" : "bg-white"
+          sender === "person-one-messages" ? "bg-message-whatsapp" : "bg-white"
         }`;
 
         return (
           <div
             key={index}
             className={`wrapper-${sender} w-50 ${
-              sender === "person-one" ? "pl-5" : "pr-5"
+              sender === "person-one-messages"
+                ? "pl-5  ml-auto mr-5"
+                : "pr-5 ml-5"
             }`}
           >
             {isNewSender && <div className="sender-indicator"></div>}
             <span key={message.createdAt}>
-              <p className={messageClasses}>{message.text}</p>
+              <p className={messageClasses}>
+                {message.text}
+                <span className="timestamp-messages">
+                  {formatTime(message.createdAt)}
+                </span>
+              </p>
             </span>
           </div>
         );
