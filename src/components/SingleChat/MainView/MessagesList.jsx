904f78fd62e2ef9e6a282c76919ac5319121792e
ConnorDW-SA <114667779+ConnorDW-SA@users.noreply.github.com>
2023-02-22 11:11:50 +0200

    Chat done needs CSS tho

--- src/components/SingleChat/MainView/MessagesList.jsx
+++ src/components/SingleChat/MainView/MessagesList.jsx
@@ -1,96 +1,42 @@
 import React from "react";
-import { Row } from "react-bootstrap";
+
+import MessagePersonOne from "../Chats/PersonOne.json";
+import MessagePersonTwo from "../Chats/PersonTwo.json";
 
 function MessagesList() {
+  let currentSender = "";
+
+  const messages = [...MessagePersonOne, ...MessagePersonTwo].sort((a, b) =>
+    a.createdAt.localeCompare(b.createdAt)
+  );
+
   return (
-    <div className="d-flex convo-div">
-      <div className="wrapper-left w-50 pl-5">
-        <span>
-          <p className="message-container p-2 m-1 bg-white text-wrap rounded-lg">
-            Lorem ipsum dolor dddddddddddddddddddddddddddd dddddddddddddddsit
-            amet.
-          </p>
-        </span>
-        <span>
-          <p className="message-container p-2 m-1 bg-white text-wrap rounded-lg">
-            Lorem ipsum dolor dddddddddddddddddddddddddddd dddddddddddddddsit
-            amet.
-          </p>
-        </span>
-        <span>
-          <p className="message-container p-2 m-1 bg-white text-wrap rounded-lg">
-            Lorem ipsum dolor dddddddddddddddddddddddddddd dddddddddddddddsit
-            amet.
-          </p>
-        </span>
-        <span>
-          <p className="message-container p-2 m-1 bg-white text-wrap rounded-lg">
-            Lorem ipsum dolor dddddddddddddddddddddddddddd dddddddddddddddsit
-            amet.
-          </p>
-        </span>
-        <span>
-          <p className="message-container p-2 m-1 bg-white text-wrap rounded-lg">
-            Lorem ipsum dolor dddddddddddddddddddddddddddd dddddddddddddddsit
-            amet.
-          </p>
-        </span>
-        <span>
-          <p className="message-container p-2 m-1 bg-white text-wrap rounded-lg">
-            Lorem ipsum dolor dddddddddddddddddddddddddddd dddddddddddddddsit
-            amet.
-          </p>
-        </span>
-        <span>
-          <p className="message-container p-2 m-1 bg-white text-wrap rounded-lg">
-            Lorem ipsum dolor dddddddddddddddddddddddddddd dddddddddddddddsit
-            amet.
-          </p>
-        </span>
-        <span>
-          <p className="message-container p-2 m-1 bg-white text-wrap rounded-lg">
-            Lorem ipsum dolor dddddddddddddddddddddddddddd dddddddddddddddsit
-            amet.
-          </p>
-        </span>
-        <span>
-          <p className="message-container p-2 m-1 bg-white text-wrap rounded-lg">
-            Lorem ipsum dolor dddddddddddddddddddddddddddd dddddddddddddddsit
-            amet.
-          </p>
-        </span>
-        <span>
-          <p className="message-container p-2 m-1 bg-white text-wrap rounded-lg">
-            Lorem ipsum dolor dddddddddddddddddddddddddddd dddddddddddddddsit
-            amet.
-          </p>
-        </span>
-        <span>
-          <p className="message-container p-2 m-1 bg-white text-wrap rounded-lg">
-            Lorem ipsum dolor dddddddddddddddddddddddddddd dddddddddddddddsit
-            amet.
-          </p>
-        </span>
-        <span>
-          <p className="message-container p-2 m-1 bg-white text-wrap rounded-lg">
-            Lorem ipsum dolor dddddddddddddddddddddddddddd dddddddddddddddsit
-            amet.
-          </p>
-        </span>
-        <span>
-          <p className="message-container p-2 m-1 bg-white text-wrap rounded-lg">
-            Lorem ipsum dolor dddddddddddddddddddddddddddd dddddddddddddddsit
-            amet.
-          </p>
-        </span>
-        <span>
-          <p className="message-container p-2 m-1 bg-white text-wrap rounded-lg">
-            Lorem ipsum dolor dddddddddddddddddddddddddddd dddddddddddddddsit
-            amet.
-          </p>
-        </span>
-      </div>
-      <div className="wrapper-right w-50 pr-5"></div>
+    <div className="d-flex flex-column convo-div">
+      {messages.map((message, index) => {
+        const sender = MessagePersonOne.includes(message)
+          ? "person-one"
+          : "person-two";
+        const isNewSender = currentSender !== sender;
+        currentSender = sender;
+
+        const messageClasses = `message-container p-2 m-1 text-wrap rounded-lg ${sender} ${
+          sender === "person-one" ? "bg-message-whatsapp" : "bg-white"
+        }`;
+
+        return (
+          <div
+            key={index}
+            className={`wrapper-${sender} w-50 ${
+              sender === "person-one" ? "pl-5" : "pr-5"
+            }`}
+          >
+            {isNewSender && <div className="sender-indicator"></div>}
+            <span key={message.createdAt}>
+              <p className={messageClasses}>{message.text}</p>
+            </span>
+          </div>
+        );
+      })}
     </div>
   );
 }
